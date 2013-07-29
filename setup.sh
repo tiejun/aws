2c3b9e602ad8db641e9afc93a79d38a8e3146c3f
Ubuntu <ubuntu@ip-172-31-1-244.us-west-2.compute.internal>
2013-07-29 04:38:30 +0000

    Use aws/dotfiles

--- setup.sh
+++ setup.sh
@@ -40,10 +40,9 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+ln -sb aws/dotfiles/.screenrc .
+ln -sb aws/dotfiles/.bash_profile .
+ln -sb aws/dotfiles/.bashrc .
+ln -sb aws/dotfiles/.bashrc_custom .
+ln -sf aws/dotfiles/.emacs.d .
 
